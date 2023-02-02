a839a96487235b4685fe9d6f4f18d54d60755414
suildesigner <105719548+suildesigner@users.noreply.github.com>
2023-02-03 04:52:29 +0900

    Add files via upload

---
+++ image_index.js
@@ -0,0 +1,578 @@
+function filter() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("listInner");
+   
+    for (let i = 0; i < listInner.length; i++) {
+        city = listInner[i].getElementsByClassName("city");
+        country = listInner[i].getElementsByClassName("country");
+        if (city[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+           country[0].innerHTML.toLowerCase().indexOf(search) != -1
+           ) {
+            listInner[i].style.display = "flex"
+        } else {
+            listInner[i].style.display = "none"
+        }
+    }
+}
+
+function filter() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("listInner");
+   
+    for (let i = 0; i < listInner.length; i++) {
+        city = listInner[i].getElementsByClassName("city");
+        country = listInner[i].getElementsByClassName("country");
+        if (city[0].innerHTML.toLowerCase().includes(search) ||
+           country[0].innerHTML.toLowerCase().includes(search)
+           ) {
+            listInner[i].style.display = "flex"
+        } else {
+            listInner[i].style.display = "none"
+        }
+    }
+}
+
+
+var t0 = document.getElementsByClassName('country')[0];
+var q0 = "떡메모지 주문하려는데 디자인 모두 다르게 할 수 있나요?"
+var a0 = "떡메모지 주문하려는데 디자인 모두 다르게 할 수 있나요?\n\n옵션 수량 당 디자인 1종류입니다.\n디자인이 여러 종류이실 경우에는 옵션 수량이 아닌\n주문 수량으로 설정해 주셔야 합니다.\n예를 들어 디자인 3종류를 5권씩 하신다면\n5권×3개로 주문해 주시면 됩니다."
+
+var t1 = document.getElementsByClassName('country')[1];
+var q1 = "포토카드는 몇 장부터 주문 가능한가요?"
+var a1 = "포토카드는 몇 장부터 주문 가능한가요?\n\n1장부터 주문 가능하며 수량에는 제한이 없습니다.\n250장 이상 대량 주문이실 경우에는 할인이 가능하니\n주문 전 상담원을 통해 문의 바랍니다."
+
+var t2 = document.getElementsByClassName('country')[2];
+var q2 = "지금 떡메모지 주문하면 언제 받을 수 있나요?"
+var a2 = "지금 떡메모지 주문하면 언제 받을 수 있나요?\n\n떡메모지 작업 기간은 평일 기준 3~5일 정도 소요됩니다.\n배송 일정에 대해서는 확답 드리기 어려운 점 참고 바라며\n급하신 건일 경우 신중한 주문 바랍니다."
+
+var t3 = document.getElementsByClassName('country')[3];
+var q3 = "배송 현황을 확인하고 싶습니다."
+var a3 = "배송 현황을 확인하고 싶습니다.\n\n배송 현황은 네이버 쇼핑 주문 내역에서 확인 가능합니다."
+
+var t4 = document.getElementsByClassName('country')[4];
+var q4 = "틴케이스, 포토카드 함께 주문 가능한가요?"
+var a4 = "틴케이스, 포토카드 함께 주문 가능한가요?\n\n포토카드 주문하실 때 추가 상품에서 주문 가능합니다."
+
+var t5 = document.getElementsByClassName('country')[5];
+var q5 = "PVC 카드와 FAPER 카드의 차이점이 궁금합니다."
+var a5 = "PVC 카드와 FAPER 카드의 차이점이 궁금합니다.\n\nPVC 카드는 플라스틱 소재이며 주민등록증 재질입니다.\n다만 소량 제작은 재전사 인쇄로 화질이 다소 떨어집니다.\nPVC 대량 제작은 옵셋 인쇄로 화질이 우수합니다.\nFAPER 카드는 플라스틱 대체 소재로\nPVC 카드와 두께가 거의 유사하며\n재전사보다 화질이 더 우수합니다.\nFAPER 카드와 PVC 옵셋은 무광도 가능합니다."
+
+var t6 = document.getElementsByClassName('country')[6];
+var q6 = "떡메모지는 1권당 몇 매인가요?"
+var a6 = "떡메모지는 1권당 몇 매인가요?\n\n1권당 100매입니다."
+
+var t7 = document.getElementsByClassName('country')[7];
+var q7 = "떡메모지 두께나 용지를 변경할 수 있나요?"
+var a7 = "떡메모지 두께나 용지를 변경할 수 있나요?\n\n변경 불가합니다.\n모조지 80g으로만 가능합니다."
+
+var t8 = document.getElementsByClassName('country')[8];
+var q8 = "머그컵은 언제부터 판매되나요?"
+var a8 = "머그컵은 언제부터 판매되나요?\n\n머그컵은 곧 상품 삭제 예정이며 재판매 계획은 없습니다."
+
+var t9 = document.getElementsByClassName('country')[9];
+var q9 = "디자인 의뢰를 하고 싶습니다."
+var a9 = "디자인 의뢰를 하고 싶습니다.\n\n디자인 의뢰는 인쇄하실 파일이 준비되지 않은\n고객님을 위한 추가 상품입니다.\n건별로 10,000원 발생하며 간단한 문구 삽입이나\n단순한 수정의 경우 좀 더 낮은 금액으로 가능합니다.\n디자인 난이도에 따라 상이할 수 있으므로\n주문 전 상담 부탁드립니다."
+
+var t10 = document.getElementsByClassName('country')[10];
+var q10 = "시안 수정은 무제한으로 할 수 있나요?"
+var a10 = "시안 수정은 무제한으로 할 수 있나요?\n\n시안 수정은 무제한이 아닌 서비스입니다.\n무리한 요청이나 횟수가 많아질 경우\n수정에 제한이 따를 수 있으니 신중하게 파일 전달 바랍니다.\n상단에 시안 미리 보기 기능은 제한 없이 사용 가능합니다.\n꼼꼼한 확인이 필요하실 경우 접수 전 이용 부탁드립니다."
+
+var t11 = document.getElementsByClassName('country')[11];
+var q11 = "대량 주문 기준이 궁금합니다."
+var a11 = "대량 주문 기준이 궁금합니다.\n\n떡메모지와 명함은 이미 할인 적용된 상품입니다.\n그 외에 포토카드, 틴케이스, 스마트톡 등\n개별 수량으로 이루어진 상품의 경우\n250건 이상 주문 시 할인 가능합니다.\n주문 상황에 따라 할인율은 달라질 수 있으니\n주문 전 상담원을 통해 문의해 주시기 바랍니다."
+
+var t12 = document.getElementsByClassName('country')[12];
+var q12 = "일러스트 가이드 파일을 다운로드하고 싶습니다."
+var a12 = "일러스트 가이드 파일을 다운로드하고 싶습니다.\n\n공공 디자인 카카오톡 채널을 통해 다운로드 가능합니다."
+
+var t13 = document.getElementsByClassName('country')[13];
+var q13 = "재단선은 몇 mm인가요?"
+var a13 = "재단선은 몇 mm인가요?\n\n떡메모지, 포토카드, 틴케이스, 스마트톡은 4mm이며\n명함은 2mm입니다.\n예를 들어 떡메모지 80×80mm일 경우\n84×84mm로 작업해야 합니다."
+
+var t14 = document.getElementsByClassName('country')[14];
+var q14 = "권장 해상도는 몇 dpi인가요?"
+var a14 = "권장 해상도는 몇 dpi인가요?\n\n300dpi입니다."
+
+var t15 = document.getElementsByClassName('country')[15];
+var q15 = "배송지를 변경하고 싶습니다."
+var a15 = "배송지를 변경하고 싶습니다.\n\n네이버 쇼핑 주문 내역에서 변경 가능합니다.\n다만 상담원 문의 없이 변경하실 경우\n기존 배송지로 배송될 수 있습니다."
+
+var t16 = document.getElementsByClassName('country')[16];
+var q16 = "급한 주문입니다. 퀵서비스로 받을 수 있을까요?"
+var a16 = "급한 주문입니다. 퀵서비스로 받을 수 있을까요?\n\n포토카드의 경우 주문하실 때 퀵서비스로 변경 가능합니다.\n그 외 상품은 상담원을 통해 문의해 주시기 바랍니다."
+
+var t17 = document.getElementsByClassName('country')[17];
+var q17 = "방문 수령이 가능한가요?"
+var a17 = "방문 수령이 가능한가요?\n\n방문 수령은 불가합니다.\n급하신 건이라면 퀵서비스를 이용해 주세요."
+
+
+function qnaStart() {
+    t0.innerText = q0;
+    t1.innerText = q1;
+    t2.innerText = q2;
+    t3.innerText = q3;
+    t4.innerText = q4;
+    t5.innerText = q5;
+    t6.innerText = q6;
+    t7.innerText = q7;
+    t8.innerText = q8;
+    t9.innerText = q9;
+    t10.innerText = q10;
+    t11.innerText = q11;
+    t12.innerText = q12;
+    t13.innerText = q13;
+    t14.innerText = q14;
+    t15.innerText = q15;
+    t16.innerText = q16;
+    t17.innerText = q17;
+}
+
+function qna0() {
+    if(t0.innerText === a0) {
+        t0.innerText = q0;
+    } else {
+        t0.innerText = a0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna1() {
+    if(t1.innerText === a1) {
+        t1.innerText = q1;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = a1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna2() {
+    if(t2.innerText === a2) {
+        t2.innerText = q2;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = a2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna3() {
+    if(t3.innerText === a3) {
+        t3.innerText = q3;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = a3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna4() {
+    if(t4.innerText === a4) {
+        t4.innerText = q4;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = a4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna5() {
+    if(t5.innerText === a5) {
+        t5.innerText = q5;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = a5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna6() {
+    if(t6.innerText === a6) {
+        t6.innerText = q6;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = a6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna7() {
+    if(t7.innerText === a7) {
+        t7.innerText = q7;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = a7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna8() {
+    if(t8.innerText === a8) {
+        t8.innerText = q8;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = a8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna9() {
+    if(t9.innerText === a9) {
+        t9.innerText = q9;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = a9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna10() {
+    if(t10.innerText === a10) {
+        t10.innerText = q10;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = a10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna11() {
+    if(t11.innerText === a11) {
+        t11.innerText = q11;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = a11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna12() {
+    if(t12.innerText === a12) {
+        t12.innerText = q12;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = a12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna13() {
+    if(t13.innerText === a13) {
+        t13.innerText = q13;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = a13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna14() {
+    if(t14.innerText === a14) {
+        t14.innerText = q14;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = a14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna15() {
+    if(t15.innerText === a15) {
+        t15.innerText = q15;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = a15;
+        t16.innerText = q16;
+        t17.innerText = q17;
+    }
+}
+
+function qna16() {
+    if(t16.innerText === a16) {
+        t16.innerText = q16;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = a16;
+        t17.innerText = q17;
+    }
+}
+
+function qna17() {
+    if(t17.innerText === a17) {
+        t17.innerText = q17;
+    } else {
+        t0.innerText = q0;
+        t1.innerText = q1;
+        t2.innerText = q2;
+        t3.innerText = q3;
+        t4.innerText = q4;
+        t5.innerText = q5;
+        t6.innerText = q6;
+        t7.innerText = q7;
+        t8.innerText = q8;
+        t9.innerText = q9;
+        t10.innerText = q10;
+        t11.innerText = q11;
+        t12.innerText = q12;
+        t13.innerText = q13;
+        t14.innerText = q14;
+        t15.innerText = q15;
+        t16.innerText = q16;
+        t17.innerText = a17;
+    }
+}
